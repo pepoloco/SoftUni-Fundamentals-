1f944516756b5a19d8bc5764bcbb67f65edefc9f
Pepo Banchev <banchevpepo@gmail.com>
2022-10-14 21:18:40 +0300

    no message

---
+++ Data Types And Variables/17-requiredReading.js
@@ -0,0 +1,7 @@
+function requiredReading(numberOfPages, pagesReadPerHour, daysToRead) {
+
+    let result = numberOfPages / pagesReadPerHour / daysToRead;
+    console.log(result);
+
+}
+requiredReading(212, 20, 2);
