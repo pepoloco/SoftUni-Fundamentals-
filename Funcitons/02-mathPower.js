e0ca98db7510c44bcd376e1b572a304cf07716e9
Pepo Banchev <56348332+pepoloco@users.noreply.github.com>
2022-10-30 00:04:12 +0300

    Add files via upload

---
+++ Funcitons/02-mathPower.js
@@ -0,0 +1,10 @@
+function mathPower(number,power){
+
+    let result = 1;
+    for (let i = 0; i < power; i++){
+     result = result * number
+    }
+    console.log(result);
+ }
+ mathPower(2,8);
+ mathPower(3,4);
