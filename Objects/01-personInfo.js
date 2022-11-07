e461b05095412027d024ba26cf96e3d307b5314e
Pepo Banchev <56348332+pepoloco@users.noreply.github.com>
2022-11-07 18:51:16 +0200

    Add files via upload

---
+++ Objects/01-personInfo.js
@@ -0,0 +1,10 @@
+function personInfo(firstName, lastName, age) {
+    let result = {};
+    result.firstName = firstName;
+    result.lastName = lastName;
+    result.age = age;
+
+    return result;
+
+}
+console.log(personInfo("Peter", "Pan", "20"));
