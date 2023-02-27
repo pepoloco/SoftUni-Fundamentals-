1c7dd7793c7f97e7aef12659abda45bd59ff154d
Pepo Banchev <56348332+pepoloco@users.noreply.github.com>
2023-02-28 00:18:28 +0200

    Update 05-blackFlag.js

--- Exam Preparation N1/05-blackFlag.js
+++ Exam Preparation N1/05-blackFlag.js
@@ -1,34 +1,42 @@
-function solve(data) {
-    //days the pirating lasts (5)
-    //plunder for a day (40)
-    //last receive the expected plunder (100)
-    //calculate how much plunder the pirates manage to gather // each day they gather the plunder
-    //attack every third day > gain 50% of the daily plunder which is (40)
-    //every fifth day fight a warship > lose 30% of total plunder
+//check if target plunder reached
+function solve(arr) {
+    let count = 1;
+    let days = arr.shift(); //days in sea
+    let dailyPlunder = +arr.shift(); //daily
+    const expectedPlunder = arr.shift(); //expected
+    let gathering = 0;
 
-    const days = Number(data.shift());
-    let dailyPlunder = Number(data.shift());
-    const expectedPlunder = Number(data.shift());
-    let totalPlunder = 0;
-
-
-    for (let i = 1; i <= days ; i++) {
-        totalPlunder += dailyPlunder;
-
-        if (i === 3) {
-            totalPlunder = (50 / 100) * dailyPlunder + dailyPlunder;
-    
-        } if (i === 5) {
-            totalPlunder = Math.abs(totalPlunder * (30 / 100) - totalPlunder);
-        } 
+    while (days !== 0) {
+        gathering += dailyPlunder;
+        if (count % 3 === 0) {
+            gathering += (dailyPlunder * 0.50);
+        } if (count % 5 === 0) {
+            gathering -= gathering * 0.30;
+        }
+        count++;
+        days--;
     }
-    let percentage = totalPlunder / expectedPlunder * 100;
-    if (totalPlunder >= expectedPlunder) {
-        console.log(`Ahoy! ${totalPlunder.toFixed(2)} plunder gained.`);
+    let percentage = (gathering / expectedPlunder) * 100;
+    if (gathering >= expectedPlunder) {
+        console.log(`Ahoy! ${gathering} plunder gained.`);
     } else {
         console.log(`Collected only ${percentage.toFixed(2)}% of the plunder.`);
     }
 
 }
-solve((["5", "40", "100"]));
-solve((["10", "20", "380"]));
+solve((["5",
+    "40",
+    "100"]));
+solve((["10",
+    "20",
+    "380"]));
+
+/*Calculate how much plunder the pirates manage to gather. Each day they gather the plunder. 
+Keep in mind that they attack more ships every third day and add additional plunder to their total gain, which is 50% of the daily plunder.
+ Every fifth day the pirates encounter a warship, and after the battle, they lose 30% of their total plunder.
+If the gained plunder is more or equal to the target, print the following:
+"Ahoy! {totalPlunder} plunder gained."
+If the gained plunder is less than the target.Calculate the percentage left and print the following:
+"Collected only {percentage}% of the plunder."
+Both numbers should be formatted to the 2nd decimal place.
+*/
