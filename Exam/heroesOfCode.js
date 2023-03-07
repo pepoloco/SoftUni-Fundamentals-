2acbf334bc893aebd7be7ee00a6c1214e0a1fd08
Pepo Banchev <banchevpepo@gmail.com>
2023-03-07 20:59:00 +0200

    no message

--- Exam/heroesOfCode.js
+++ Exam/heroesOfCode.js
@@ -1,81 +1,72 @@
 function solve(arr) {
-    let heroesNum = arr.shift();
+
+    let numOfHeroes = arr.shift();
     let heroes = {};
-
-    while (heroesNum > 0) {
-        let [heroName, mp, hp] = arr.shift().split(' ')
-        heroes[heroName] = {
-            mp: +mp,
-            hp: +hp,
-        };
-        heroesNum--;
-    };
-
-    const castSpell = function (heroName, mpNeed, spellName) {
-        mpNeed = +(mpNeed);
-        if (heroes[heroName].mp - mpNeed > 0) {
-            heroes[heroName].mp -= mpNeed;
-            console.log(`${heroName} has successfully cast ${spellName} and now has ${heroes[heroName].mp} MP!`);
-        } else {
-            console.log(`${heroName} does not have enough MP to cast ${spellName}!`);
-        };
-    };
-    const heal = function () {
-
-    };
-    const recharge = function (heroName, amount) {
-        amount = +(amount);
-        heroName[heroName].mp = 200;
-        console.log(`${heroName} recharged for ${amount}`);
-    };
-    const takeDamage = function (heroName, damage, attacker) {
-        damage = +(damage);
-        if (heroes[heroName].hp - damage > 0) {
-            heroes[heroName].hp -= damage;
-            console.log(`${heroName} was hit for ${damage} HP by ${attacker} and now has ${heroes[heroName].hp} HP left!`);
-        } else {
-            console.log(`${heroName} has been killed by ${attacker}!`);
-        }
-    };
-
+    while (numOfHeroes > 0) {
+        let [heroName, hp, mp] = arr.shift().split(' ')
+        heroes[heroName] = { hp: +hp, mp: +mp }
+        numOfHeroes--;
+    }
     let command = arr.shift();
 
     while (command !== 'End') {
-        let action = command.split(' - ');
-        switch (action[0]) {
-            case 'CastSpell':
-                castSpell(action[1], action[2], action[3]);
-                break;
-            case 'TakeDamage':
-                takeDamage(action[1], action[2], action[3]);
-                break;
-            case 'Recharge':
-                recharge(action[1], action[2]);
-                break;
-            case 'Heal':
-                heal(action[1], action[2]);
-                break;
+        let [action, heroName, arg1, arg2] = command.split(' - ');
+        switch (action) {
+            case ('CastSpell'): castSpell(heroName, arg1, arg2); break;
+            case ("TakeDamage"): takeDamage(heroName, arg1, arg2); break;
+            case ("Recharge"): recharge(heroName, arg1); break;
+            case ("Heal"): heal(heroName, arg1); break;
         }
+
+
         command = arr.shift();
     }
-    let nameSort = Object.entries(heroes).sort((a, b) => a[0].localeCompare(b[0]));
-    let hpSort = nameSort.sort((a, b) => b[1].hp - a[1].hp);
-
-
-    for (let kvp of hpSort) {
+    function castSpell(name, mp, spell) {
+        mp = +mp;
+        if (heroes[name].mp - mp > 0) {
+            heroes[name].mp -= mp;
+            console.log(`${name} has successfully cast ${spell} and now has ${heroes[name].mp} MP!`);
+        } else {
+            console.log(`${name} does not have enough MP to cast ${spell}`);
+        }
+    };
+    function takeDamage(name, damage, attacker) {
+        damage = +damage;
+        if (heroes[name].hp - damage > 0) {
+            console.log(`${name} was hit for ${damage} HP by ${attacker} and now has ${heroes[name].hp} HP left!`);
+        } else {
+            console.log(`${name} has been killed by ${attacker}`);
+        }
+    };
+    function recharge(name, amount) {
+        amount = +amount;
+        heroes[name].mp += amount;
+        if (heroes[name].mp > 200) {
+            heroes[name].mp = 200;
+        }
+        console.log(`${name} recharged for ${amount} MP!`);
+    }
+    function heal(name, amount) {
+        amount = +amount
+        heroes[name].hp += amount;
+        if (heroes[name].hp > 100) {
+            heroes[name].hp = 100;
+        }
+        console.log(`${name} healed for ${amount} HP!`);
+    };
+    let x = Object.entries(heroes);
+    for (let kvp of x) {
         console.log(kvp[0]);
         console.log(`  HP: ${kvp[1].hp}`);
         console.log(`  MP: ${kvp[1].mp}`);
     }
 }
-solve([
-    '2',
+solve([2,
     'Solmyr 85 120',
     'Kyrre 99 50',
     'Heal - Solmyr - 10',
     'Recharge - Solmyr - 50',
     'TakeDamage - Kyrre - 66 - Orc',
     'CastSpell - Kyrre - 15 - ViewEarth',
-    'End'
-]);
+    'End']);
 
